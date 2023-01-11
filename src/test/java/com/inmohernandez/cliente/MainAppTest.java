0e7fe1e5f96fc26d9f5977e4a06b09e01e97aff4
Samir Akrouh <akrouhsamir@gmail.com>
2023-01-11 09:48:12 +0100

    16º Commit intento de testFX a altaInmueble 2

--- src/test/java/com/inmohernandez/cliente/MainAppTest.java
+++ src/test/java/com/inmohernandez/cliente/MainAppTest.java
@@ -1,11 +1,15 @@
 package com.inmohernandez.cliente;
 
+import com.inmohernandez.cliente.models.Inmueble;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.MouseButton;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.testfx.api.FxAssert;
@@ -14,9 +18,13 @@ import org.testfx.framework.junit5.ApplicationExtension;
 import org.testfx.framework.junit5.Start;
 import org.testfx.matcher.base.WindowMatchers;
 import org.testfx.matcher.control.TextInputControlMatchers;
+import org.testfx.service.query.PointQuery;
 
 import java.io.IOException;
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.*;
 @ExtendWith(ApplicationExtension.class)
@@ -35,38 +43,46 @@ class MainAppTest {
 
     @Test
     public void testAltaInmueble(FxRobot robot) throws InterruptedException {
-        Button btnCrear = robot.lookup("#btnCrear").queryAs(Button.class);
-        robot.clickOn(btnCrear);
+        final String titulo = "Apartamento en Balerma Nuevo";
+
+
+        robot.clickOn("#btnCrear");
         robot.sleep(1000);
         FxAssert.verifyThat(robot.window("Crear inmueble"), WindowMatchers.isShowing());
 
-        // Buscar el campo de texto "tfTitulo" en la interfaz gráfica
-        TextField tfTitulo = robot.lookup("#tfTitulo").queryAs(TextField.class);
-        // Hacer click en el campo de texto y escribir "Daddy Yanke"
-        robot.clickOn(tfTitulo).write("Daddy Yanke");
+        robot.clickOn("#tfTitulo").write(titulo);
 
         DatePicker datePublicacion = robot.lookup("#datePublicacion").queryAs(DatePicker.class);
+        datePublicacion.getEditor().setText("19/08/2019");
 
-        // Hacer click en el botón de flecha (arrow) del datepicker
-        robot.interact(()-> {
-            datePublicacion.show();
-        });
-        robot.sleep(1000);
+        robot.clickOn("#tfPrecio").write("125650");
 
-        Button leftArrow = robot.lookup(".date-picker-year-button").queryAs(Button.class);
-        robot.clickOn(leftArrow);
+        robot.clickOn("#cboxZona").clickOn("Balerma");
 
 
+        robot.clickOn("#tfUbicacion").write("Calle Carlomagno");
 
+        robot.clickOn("#spinnerHabitaciones").type(KeyCode.UP).type(KeyCode.UP).type(KeyCode.UP);
+        robot.clickOn("#spinnerBannos").type(KeyCode.UP).type(KeyCode.UP);
 
-        // Buscar el campo de texto "tfPrecio" en la interfaz gráfica
-        TextField tfPrecio = robot.lookup("#tfPrecio").queryAs(TextField.class);
-        // Hacer click en el campo de texto y escribir "125650"
-        robot.clickOn(tfPrecio).write("125650");
+        robot.clickOn("#tfMetros").write("150");
+        robot.clickOn("#tfMetrosUtiles").write("150");
 
-        // Buscar el combobox "cboxZona" en la interfaz gráfica
-        ComboBox cboxZona = robot.lookup("#cboxZona").queryAs(ComboBox.class);
-        // Hacer click en el combobox y seleccionar el item 1
-        robot.clickOn(cboxZona).clickOn(String.valueOf(1));
+        robot.clickOn("#taDescripcion").write("Buen apartamento de venta chulisimo");
+
+        robot.clickOn("#btnCrearInmueble");
+
+        TableView tableView = robot.lookup("#tvInmuebles").queryAs(TableView.class);
+        int lastIndex = tableView.getItems().size() - 1;
+        robot.interact(() -> tableView.scrollTo(lastIndex));
+        robot.interact(() -> tableView.getSelectionModel().select(lastIndex));
+        Inmueble inmueble = (Inmueble)tableView.getSelectionModel().getSelectedItem();
+
+        Assertions.assertEquals(titulo,inmueble.getTitulo());
+        Assertions.assertEquals();
     }
+
+
+
+
 }
